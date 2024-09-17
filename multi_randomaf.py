e9c0c0363d8f40f01d68e02d8fe9e156dfaac244
Tabitha Doicin <atdoicin@gmail.com>
2024-09-17 23:51:19 +0100

    Update multi_randomaf.py

--- multi_randomaf.py
+++ multi_randomaf.py
@@ -18,21 +18,21 @@ import concurrent.futures
 import pickle
 
 N=24
-D1=2
-D2=2
+D1=3
+D2=3
 ep1=0.2
 ep2=0.2
 wc=1
 wa=1
 geff_list_min = 0
 geff_list_max = 2.6
-geff_list_num = 50
+geff_list_num = 250
 geff_list = np.linspace(geff_list_min, geff_list_max, geff_list_num) 
 additionscaling = [k**2/wc for k in geff_list]
 lines = N*(D1+D2)
 
 sets = 4
-nsys = 150
+nsys = 100
 normalised = True
 
 def create_dataframe(nsys):
