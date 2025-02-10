76b1689baacc6ff92721dd2590a23fa5c8ad733f
Tabitha Doicin <atdoicin@gmail.com>
2025-02-10 19:43:22 +0000

    goop

--- multi_randomaf.py
+++ multi_randomaf.py
@@ -19,8 +19,8 @@ import pickle
 import time
 
 N=24
-D1=8
-D2=8
+D1=3
+D2=3
 ep1=0.1
 ep2=0.1
 wc=1
@@ -32,9 +32,9 @@ geff_list = np.linspace(geff_list_min, geff_list_max, geff_list_num)
 additionscaling = [k**2/wc for k in geff_list]
 lines = N*(D1+D2)
 
-sets = 4
-nsys = 150
-nlines = 80
+sets = 8
+nsys = 100
+nlines = 40
 normalised = True
 
 def create_dataframe(nsys):
